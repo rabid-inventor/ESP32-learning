64465bfd3148da3bef90f7c4ec6aacd1972d5de2
rabid-inventor <geeus81@gmail.com>
2018-04-25 17:34:25 +0100

    Added new project for ble-mesh networking

---
+++ ble-mesh/main/ble_config.h
@@ -0,0 +1,80 @@
+
+/* hearder for defining GAP datasets */
+#include "esp_system.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_ble_api.h"
+#include "esp_bt_defs.h"
+
+#define MY_DEMO_TAG "BT demo Gee"
+
+#define TEST_DEVICE_NAME "DEV01"
+
+uint8_t dev_name[5] = TEST_DEVICE_NAME;
+
+
+typedef struct{
+	uint8_t 	device_name_len;
+	uint8_t		*p_device_name;
+
+}device_details_t;
+
+
+device_details_t my_device_details = {
+		.device_name_len = sizeof(dev_name),
+		.p_device_name = dev_name,
+
+};
+
+static uint8_t adv_service_uuid128[32] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xEE, 0x00, 0x00, 0x00,
+    //second uuid, 32bit, [12], [13], [14], [15] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+};
+
+
+static esp_ble_adv_data_t default_adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = true,
+    .min_interval = 0x20,
+    .max_interval = 0x40,
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = 32,
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+static esp_ble_adv_data_t scan_rsp_data = {
+    .set_scan_rsp = true,
+    .include_name = true,
+    .include_txpower = true,
+    .min_interval = 0x20,
+    .max_interval = 0x40,
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = 32,
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+static esp_ble_adv_params_t default_test_adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x40,
+    .adv_type           = ADV_TYPE_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    //.peer_addr        =
+    //.peer_addr_type   =
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy  = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+
---
+++ ble-mesh/main/main.c
@@ -0,0 +1,131 @@
+
+
+
+#include "ble_system.h"
+//#include "ble_config.h"
+#include "esp_gap_ble_api.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+//#include "esp_bt.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+
+
+#include "esp_gap_ble_api.h"
+
+
+
+static uint8_t adv_config_done = 0;
+#define adv_config_flag      (1 << 0)
+#define scan_rsp_config_flag (1 << 1)
+
+
+// Initises Bluetooth controller 
+
+
+//Handler for advertising 
+void gap_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param){
+ ESP_LOGI(MY_DEMO_TAG, "BLE Event detected");
+switch (event) {
+		
+    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+         adv_config_done &= (~adv_config_flag);
+         if (adv_config_done == 0){
+             esp_ble_gap_start_advertising(&default_test_adv_params);
+         }
+         
+		 break;
+		 
+    case ESP_GAP_BLE_SCAN_RSP_DATA_SET_COMPLETE_EVT:
+         adv_config_done &= (~scan_rsp_config_flag);
+         if (adv_config_done == 0){
+             esp_ble_gap_start_advertising(&default_test_adv_params);
+         }
+         
+		 break;
+    
+	case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+    //advertising start complete event to indicate advertising start successfully or failed
+         if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+             ESP_LOGE(MY_DEMO_TAG, "Advertising start failed\n");
+         }
+         
+		 break;
+	case ESP_GAP_BLE_UPDATE_CONN_PARAMS_EVT:
+
+	       ESP_LOGI(MY_DEMO_TAG, "update connection params status = %d, min_int = %d, max_int = %d,conn_int = %d,latency = %d, timeout = %d",
+	                  param->update_conn_params.status,
+	                  param->update_conn_params.min_int,
+	                  param->update_conn_params.max_int,
+	                  param->update_conn_params.conn_int,
+	                  param->update_conn_params.latency,
+
+					  param->update_conn_params.timeout);
+	       conn_params_t new_connetion_params;
+
+	       //esp_ble_gap_update_conn_params(&update_conn_params);
+		ESP_LOGI(MY_DEMO_TAG, "Set con params recieved need to do\n");
+	    //ESP_LOGI(MY_DEMO_TAG," event: %x params: %x /n", event , param);
+	    break;
+    default:
+	    //ESP_LOGI(MY_DEMO_TAG," event: %x params: %x /n", event , param); 
+	     break;
+}
+
+}
+
+
+
+
+void app_main()
+	{
+	
+	ble_begin();
+	
+	//Initiase BlueTooth stack
+	
+	ESP_BLUEDROID_STATUS_CHECK(ESP_BT_STATUS_DONE)
+
+	esp_err_t set_dev_name_ret = esp_ble_gap_set_device_name(TEST_DEVICE_NAME);
+	
+	if (set_dev_name_ret){
+		ESP_LOGE(MY_DEMO_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+	}
+	
+	
+	
+	esp_err_t ret = esp_ble_gap_config_adv_data(&default_adv_data);
+	
+	if (ret){
+		ESP_LOGE(MY_DEMO_TAG, "config adv data failed, error code = %x", ret);
+	}
+	adv_config_done |= adv_config_flag;
+	
+	//config scan response data
+	ret = esp_ble_gap_config_adv_data(&scan_rsp_data);
+	
+	if (ret){
+		ESP_LOGE(MY_DEMO_TAG, "config scan response data failed, error code = %x", ret);
+	}
+	adv_config_done |= scan_rsp_config_flag;
+
+    ret = esp_ble_gap_register_callback(gap_handler);
+	
+    if (ret){
+        ESP_LOGE(MY_DEMO_TAG, "gap register error, error code = %x", ret);
+        return;
+    }
+	 ESP_LOGI(MY_DEMO_TAG, "GAP CB Up");
+	 
+	     if (adv_config_done == 0){
+             esp_ble_gap_start_advertising(&default_test_adv_params);
+			 ESP_LOGI(MY_DEMO_TAG, "Advert start");
+         }
+		 
+		 ESP_ERROR_CHECK(esp_ble_gap_start_advertising(&default_test_adv_params));
+		 
+         
+}
